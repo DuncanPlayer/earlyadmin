95f638163e8be16b21a39b957ff84ea0a618c429
DuncanPlayer <1540014394@qq.com>
2020-04-15 09:23:41 +0800

    war部署

--- src/main/java/net/messi/earlyadmin/EarlyadminApplication.java
+++ src/main/java/net/messi/earlyadmin/EarlyadminApplication.java
@@ -2,14 +2,20 @@ package net.messi.earlyadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("net.messi.earlyadmin.mapper")
-public class EarlyadminApplication {
+public class EarlyadminApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(EarlyadminApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EarlyadminApplication.class);
+    }
 }
